d4b5930d740004d541ec9d5993295dc819594edf
Thomas Deruel <thomas.deruel@hetic.net>
2018-06-27 12:21:01 +0200

    fusion

---
+++ js/table.js
@@ -0,0 +1,23 @@
+let planette = [{
+    question: 'Vous préférez vivre?:',
+    answers: ['un dome', 'Sous terre', 'Sous une atmosphere présente',],
+}, {
+    question: 'Quel climat souhaitez-vous?:',
+    answers: ['Aride', 'Tropicale', 'Tempéré', 'Continental','Montagnard','Polaire'],
+}, {
+    question: 'Quel environement vous convient le mieux?:',
+    answers: ['eau', 'vegetal', 'montagneux', 'desertique','Roquailleux','Polaire'],
+},{
+    question: 'Quel niveau de technologie souhaitez-vous?:',
+    answers: ['Avancé', 'Moyen', 'Absent'],
+},{
+    question: 'Quelle intensité lumineuse souhaitez-vous?:',
+    answers: ['importante', 'Moyenne', 'Peu', 'Absente']
+},{
+    question: 'Quel niveau de densité de population souhaitez-vous?:',
+    answers: ['Très forte', 'Forte', 'Moyenne', 'Peu']
+},{
+    question: 'quel niveau de gravité souhaitez vous?:',
+    answers: ['élevé', 'equivalente a celle de la terre', 'faible'],
+
+}];
